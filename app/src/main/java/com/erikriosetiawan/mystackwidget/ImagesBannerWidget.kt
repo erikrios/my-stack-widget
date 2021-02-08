445127bbef6f0b1c597105c4a3d63c03870ede6e
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-08 12:42:17 +0700

    Refactor the code

---
+++ app/src/main/java/com/erikriosetiawan/mystackwidget/ImagesBannerWidget.kt
@@ -0,0 +1,56 @@
+package com.erikriosetiawan.mystackwidget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import androidx.core.net.toUri
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class ImagesBannerWidget : AppWidgetProvider() {
+
+    companion object {
+        private const val TOAST_ACTION = "com.erikriosetiawan.mystackwidget.TOAST_ACTION"
+        private const val EXTRA_ITEM = "com.erikriosetiawan.mystackwidget.EXTRA_ITEM"
+
+        private fun updateAppWidget(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+            val intent = Intent(context, StackWidgetService::class.java).apply {
+                putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+                data = this.toUri(Intent.URI_INTENT_SCHEME).toUri()
+            }
+
+            val view = RemoteViews(context.packageName, R.layout.image_banner_widget).apply {
+                setRemoteAdapter(R.id.stack_view, intent)
+                setEmptyView(R.id.stack_view, R.id.empty_view)
+            }
+
+            val toastIntent = Intent(context, ImagesBannerWidget::class.java)
+        }
+    }
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
